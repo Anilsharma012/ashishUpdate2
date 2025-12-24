69eda23dd606a0a5f93a5f41a4d536f29f4320bf
sachin takoria <sachintakoria2204@gmail.com>
2025-12-24 13:33:15 +0530

    dfdf

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-137dedbd'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.0933b3s6988"
+    "revision": "0.e3g1e4ddsmg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
